d06e303deabd4f21452f9c35f6a57b694cbc90b9
Harris <skyshow520@vip.qq.com>
2016-05-24 10:17:23 +0800

    获取IP地址
    获取IP地址，  包含IPV4，IPV6

---
+++ GetIpAddress(v4+v6)/GetIPAddress/ViewController.m
@@ -0,0 +1,44 @@
+//
+//  ViewController.m
+//  GetIPAddress
+//
+//  Created by Cocoa Lee on 8/28/15.
+//  Copyright (c) 2015 Cocoa Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "GetIPAddress.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *ipv4 = [GetIPAddress getIPAddress:true];
+    NSLog(@"ipv4 :  %@",ipv4);
+    NSDictionary *all = [GetIPAddress getIPAddresses];
+    NSLog(@"all  :  %@", all);
+
+}
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesBegan:touches withEvent:event];
+    
+    NSString *ipv4 = [GetIPAddress getIPAddress:true];
+    NSLog(@"ipv4 :  %@",ipv4);
+    NSDictionary *all = [GetIPAddress getIPAddresses];
+    NSLog(@"all  :  %@", all);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
